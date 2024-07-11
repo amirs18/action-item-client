696b0393cb9fca234f9e051c26d527579b3cab1c
amirs18 <amir135246@gmail.com>
2024-07-11 16:48:54 +0300

    added update

--- src/compunents/users.tsx
+++ src/compunents/users.tsx
@@ -3,6 +3,7 @@ import { UserAPI, UserResults } from "../userTypes";
 import axios from "axios";
 import UserRows from "./UserRows";
 import UserHistoryRows from "./UserHistoryRows";
+import { useState } from "react";
 
 const BACKEND_URL = "http://localhost:4000";
 
@@ -66,6 +67,7 @@ export function User({
   setUser: React.Dispatch<React.SetStateAction<UserAPI | null>>;
 }) {
   useUser(user);
+  const [nameInput, setNameInput] = useState<UserAPI["name"]>(user.name);
 
   return (
     <div>
@@ -76,9 +78,48 @@ export function User({
       </div>
       {user && (
         <>
-          <h1>{user.name.title}</h1>
-          <div>
-            <p>{user.gender}</p>
+          <div className="flex flex-col items-center align-middle">
+            <img src={user.picture.large} alt="user picture" />
+            <p>gender:{user.gender}</p>
+            <div>
+              Title:
+              <input
+                defaultValue={user.name.title}
+                onChange={(e) => {
+                  setNameInput({ ...nameInput, title: e.target.value });
+                }}
+              />
+              First:
+              <input
+                defaultValue={user.name.first}
+                onChange={(e) => {
+                  setNameInput({ ...nameInput, first: e.target.value });
+                }}
+              />
+              Last:
+              <input
+                defaultValue={user.name.last}
+                onChange={(e) => {
+                  setNameInput({ ...nameInput, last: e.target.value });
+                }}
+              />
+            </div>
+            <p>
+              age: {user.dob.age} date of birth:
+              {new Date(user.dob.date).toUTCString()}
+            </p>
+            <p>
+              <h3>Address</h3>
+              Street number {user.location.street.number}
+              {"   "}
+              Street Name: {user.location.street.name}
+            </p>
+            <p>
+              <h3>Contact</h3>
+              email {user.email}
+              {"   "}
+              Phone: {user.phone}
+            </p>
           </div>
           <button
             className="btn"
@@ -98,6 +139,14 @@ export function User({
             }}
           >
             delete
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              axios.patch(`${BACKEND_URL}/user/${user.email}`, nameInput);
+            }}
+          >
+            update
           </button>
         </>
       )}
